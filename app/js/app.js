9988a87eaef9773a9f5a3544cdb276dba610b18a
Martin Spier <spiermar@gmail.com>
2014-05-10 19:24:06 -0700

    Including Bootstrap 3.

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,15 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('flohmarktApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'flohmarktFilters',
+  'flohmarktServices',
+  'flohmarktDirectives',
+  'flohmarktControllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'flohmarktCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'flohmarktCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
