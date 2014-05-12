32da7a8c42f86ce1e17af66dbbdfac7625d409ac
Martin Spier <spiermar@gmail.com>
2014-05-11 18:35:50 -0700

    Using Angular to show a static list of items.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,8 @@
 
 /* Controllers */
 
-angular.module('flohmarktControllers', [])
-  .controller('flohmarktCtrl1', ['$scope', function($scope) {
+var flohmarktApp = angular.module('flohmarktApp', []);
 
-  }])
-  .controller('flohmarktCtrl2', ['$scope', function($scope) {
-
-  }]);
+flohmarktApp.controller('ItemListCtrl', function($scope) {
+  $scope.items = [{"price":1,"description":"test description","name":"test","_id":"536ebd787b7e86fc0ccb0d99","__v":0,"likes":[]},{"price":2,"description":"test description 2","name":"test 2","_id":"536f002a77b4ca02004ee0ec","__v":0,"likes":[]}]
+});
