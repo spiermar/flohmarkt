8ee92132e25644137710993f031fc78b679c4dca
Martin Spier <spiermar@gmail.com>
2014-05-11 19:08:53 -0700

    Loading item list from REST interface.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,8 @@
 
 var flohmarktApp = angular.module('flohmarktApp', []);
 
-flohmarktApp.controller('ItemListCtrl', function($scope) {
-  $scope.items = [{"price":1,"description":"test description","name":"test","_id":"536ebd787b7e86fc0ccb0d99","__v":0,"likes":[]},{"price":2,"description":"test description 2","name":"test 2","_id":"536f002a77b4ca02004ee0ec","__v":0,"likes":[]}]
-});
+flohmarktApp.controller('ItemListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('/REST/items').success(function(data) {
+  	$scope.items = data;
+  });
+}]);
