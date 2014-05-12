af3b2dc056fbbb50fb505f4242d68080f9551bf3
Martin Spier <spiermar@gmail.com>
2014-05-11 18:31:16 -0700

    Cleaning up modules.

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,3 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-angular.module('flohmarktApp', [
-  'ngRoute',
-  'flohmarktFilters',
-  'flohmarktServices',
-  'flohmarktDirectives',
-  'flohmarktControllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'flohmarktCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'flohmarktCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+/* App Module */
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,3 @@
 'use strict';
 
 /* Directives */
-
-
-angular.module('flohmarktDirectives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
